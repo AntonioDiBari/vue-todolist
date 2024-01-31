bfc33b76eff7324b8b03c8d035ded9b51efa9b9e
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-01-31 16:03:40 +0100

    feat: milestone 1 + 2

--- js/main.js
+++ js/main.js
@@ -3,17 +3,40 @@ const { createApp } = Vue;
 const app = createApp({
   data() {
     return {
-      isOk: "ok Vue",
+      tasks: [
+        {
+          text: "Fare la spesa",
+          state: false,
+        },
+        {
+          text: "Fare le pulizie",
+          state: false,
+        },
+        {
+          text: "Fare il bucato",
+          state: true,
+        },
+        {
+          text: "Rinnovare la patente",
+          state: false,
+        },
+        {
+          text: "Portare a spasso il cane",
+          state: false,
+        },
+        {
+          text: "Prendere la compressa",
+          state: true,
+        },
+      ],
     };
   },
   methods: {
-    printOK() {
-      console.log(this.isOk);
+    deleteTask(currentTask) {
+      this.tasks.splice(currentTask, 1);
     },
   },
-  mounted() {
-    this.printOK();
-  },
+  mounted() {},
 });
 
 app.mount("#root");
